5209e90457ef1f331b3cf39cd8d28d23897190f6
Ragavendra S <ragavendrabest77@gmail.com>
2021-04-03 18:53:13 +0530

    Product action constant reducer added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const reducers = combineReducers({})
+import { ProductListReducer } from './reducers/productListReducer'
+
+const reducers = combineReducers({
+  ProductList: ProductListReducer,
+})
 
 const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
