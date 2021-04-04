0188ea8c8adeb5c89013bd184f0abdfc35e384a1
Ragavendra S <ragavendrabest77@gmail.com>
2021-04-05 00:23:59 +0530

    Authmiddleware added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -21,4 +21,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+const getUserProfile = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.send({
+      _id: user._id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { authUser } from '../controller/userController.js'
+import { authUser, getUserProfile } from '../controller/userController.js'
+import protect from '../middleware/authMiddleware.js'
 
 const Router = express.Router()
 
 Router.route('/login').post(authUser)
+Router.route('/profile').get(protect, getUserProfile)
 
 export default Router
