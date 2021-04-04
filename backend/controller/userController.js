edaa25355f5a0b22539362f54c2b41093fecee90
Ragavendra S <ragavendrabest77@gmail.com>
2021-04-05 01:10:02 +0530

    Register User and pre save middleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -22,7 +22,6 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 const getUserProfile = asyncHandler(async (req, res) => {
-
   const user = await User.findById(req.user._id)
 
   if (user) {
@@ -37,4 +36,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exist')
+  } else {
+    const user = await User.create({ name, email, password })
+
+    if (user) {
+      res.send({
+        _id: user._id,
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(401)
+      throw new Error('Not able to create user')
+    }
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controller/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controller/userController.js'
 import protect from '../middleware/authMiddleware.js'
 
 const Router = express.Router()
 
 Router.route('/login').post(authUser)
 Router.route('/profile').get(protect, getUserProfile)
+Router.route('/').post(registerUser)
 
 export default Router
