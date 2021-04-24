2f55f8b8a30c8b0d4cb6f802197db0e1f545828f
Ragavendra S <ragavendrabest77@gmail.com>
2021-04-24 12:57:08 +0530

    Login reducer constant action added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,19 +8,26 @@ import {
 } from './reducers/productReducer'
 
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducers = combineReducers({
   ProductList: ProductListReducer,
   ProductDetails: ProductDetailsReducer,
   cart: cartReducer,
+  user: userLoginReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : []
+
 const initialState = {
   cart: { cartItems: cartItemFromStorage },
+  userLoad: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
