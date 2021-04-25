1593c67cf079466043919120d87e29bcfcbb6a68
Ragavendra S <ragavendrabest77@gmail.com>
2021-04-25 13:58:51 +0530

    Update user profile endpoint added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -30,11 +30,35 @@ const getUserProfile = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      isAdmin: user.isAdmin
     })
   } else {
     res.status(404)
     throw new Error('User not found')
+  }
+})
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+    const updateUser = await user.save()
+
+    res.send({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
   }
 })
 
@@ -65,4 +89,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser }
+export { authUser, getUserProfile, registerUser, updateUserProfile }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controller/userController.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controller/userController.js'
 import protect from '../middleware/authMiddleware.js'
 
 const Router = express.Router()
 
 Router.route('/login').post(authUser)
-Router.route('/profile').get(protect, getUserProfile)
+Router.route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 Router.route('/').post(registerUser)
 
 export default Router
