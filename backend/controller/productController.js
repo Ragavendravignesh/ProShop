99573eb63f6b466d43742e9173865bd33c73f538
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-03 19:57:29 +0530

    Review end point added

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -56,28 +56,81 @@ const createProduct = asyncHandler(async (req, res) => {
 const updateProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
-  const {name, image, description, brand, category, price, countInStock, rating, numReviews } = req.body
+  const {
+    name,
+    image,
+    description,
+    brand,
+    category,
+    price,
+    countInStock,
+    rating,
+    numReviews,
+  } = req.body
 
   if (product) {
-      product.user = req.user.id || product.id,
-      product.name = name || product.name,
-      product.image = image || product.image,
-      product.description = description || product.description,
-      product.brand = brand || product.brand,
-      product.category = category || product.category,
-      product.price = price || product.price,
-      product.countInStock = countInStock || product.countInStock,
-      product.rating = rating || product.rating,
-      product.numReviews = numReviews || product.numReviews
+    ;(product.user = req.user.id || product.id),
+      (product.name = name || product.name),
+      (product.image = image || product.image),
+      (product.description = description || product.description),
+      (product.brand = brand || product.brand),
+      (product.category = category || product.category),
+      (product.price = price || product.price),
+      (product.countInStock = countInStock || product.countInStock),
+      (product.rating = rating || product.rating),
+      (product.numReviews = numReviews || product.numReviews)
 
-      const updatedProduct = await product.save()
+    const updatedProduct = await product.save()
 
-      res.json(updatedProduct)
-    }
-    else {
-      res.status(401)
-      throw new Error('Product not found')
-    }
+    res.json(updatedProduct)
+  } else {
+    res.status(401)
+    throw new Error('Product not found')
+  }
 })
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    )
+
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed')
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    }
+
+    product.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+
+    res.status(201).json({message : 'Review added'})
+  } else {
+    res.status(401)
+    throw new Error('Product not found')
+  }
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from '../controller/productController.js'
+import { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview } from '../controller/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const Router = express.Router()
 
 Router.route('/').get(getProducts).post(protect, admin, createProduct)
+Router.route('/:id/reviews').post(protect, createProductReview)
 Router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default Router
