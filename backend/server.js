2fdfc83c53dfdc5c8f7b4b42fcaa71c05b5d45a9
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-03 19:57:05 +0530

    Morgan logger added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import path from 'path'
+import morgan from 'morgan'
 
-import {notFound, errorHandler } from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productsRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -18,6 +19,10 @@ const app = express()
 
 app.use(express.json())
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.get('/', (req, res) => {
   res.send('API is Running...')
 })
@@ -27,13 +32,15 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID ))
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
